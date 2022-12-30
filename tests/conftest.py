2783a5136d09af313e0d3af42ceaddc9b9f82315
dudick123 <brian.dudick@outlook.com>
2022-12-30 14:38:17 -0500

    more

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,6 @@
 import pytest
 import requests
 
-<<<<<<< HEAD
-@pytest.fixture(scope='session')
-def login_as_admin():
-    url = 'http://localhost:8080/auth/login'
-    payload = {'username': 'admin', 'password': 'admin'}
-    response = requests.post(url, data = payload)
-    assert response.status_code == 200
-    access_token = response.json()['access_token']
-    yield access_token
-=======
 @pytest.fixture(scope="session")
 def login_as_admin():
     payload = {"username": "admin", "password": "admin"}
@@ -19,4 +9,3 @@ def login_as_admin():
 
     access_token = response.json()['access_token']
     yield access_token
->>>>>>> origin/main
